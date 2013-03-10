3c24a9ea13f44600d4737b4b080961903c4a43aa
Joshua Snyder <jsnyder28@fullsail.edu>
2013-03-10 01:19:33 -0600

    Added shell functions for the submit, reset, and clear buttons.

--- js/main.js
+++ js/main.js
@@ -2,3 +2,21 @@
 // ASD 1303
 // Project Week 1
 
+
+$("#newRcpForm").on("pageinit", function() {
+	//When form page loads, validate function runs.
+	
+});
+
+$("#addIt").on("click", function() {
+	//Saves current data from input fields of form into storage.
+});
+
+$("resetIt").on("click", function() {
+	//Resets all input in form fields.
+});
+
+
+$("#resetAll").on("click", function() {
+	//Clears all currently saved data.
+});
