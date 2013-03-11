f9db60c446e2edb770c25e102d0dfd4ba4028389
Joshua Snyder <jsnyder28@fullsail.edu>
2013-03-11 17:51:44 -0500

    Routine commit.

--- js/main.js
+++ js/main.js
@@ -3,20 +3,40 @@
 // Project Week 1
 
 
+$("#home").on("pageinit", function() {
+	/* $('<ul>').appendTo('#home section');
+	// Creates ul for categories in the homepage and appends it to the page
+	$('<li><a href="#american">American</a></li>').appendTo('#home section ul');
+	// Creates the li and anchor tags for the individual categories and appends them to the ul */
+	return false;
+});
+// END Method
+
 $("#newRcpForm").on("pageinit", function() {
 	//When form page loads, validate function is active.
-	
+
+	return false;
 });
+// END Method
 
 $("#addIt").on("click", function() {
 	//Saves current data from input fields of form into storage.
+
+	return false;
 });
+// END Method
 
 $("resetIt").on("click", function() {
 	//Resets all input in form fields.
+
+	return false;
 });
+// END Method
 
 
 $("#resetAll").on("click", function() {
 	//Clears all currently saved data.
+
+	return false;
 });
+// END Method
