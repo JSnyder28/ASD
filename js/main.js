df7402c1e0ecf3cab98e0ecdccf30bd7125d8ab1
Joshua Snyder <jsnyder28@fullsail.edu>
2013-03-11 19:10:22 -0500

    Wrote js code that dynamically creates the page, and everything in it, for the American category.

--- js/main.js
+++ js/main.js
@@ -3,11 +3,28 @@
 // Project Week 1
 
 
-$("#home").on("pageinit", function() {
+$('#home').on('pageinit', function() {
+	
+});
+// END Method
+
+/*$('#home').on('pageinit', function() {
+	$('#american').on('click', function() {
+		$('<section data-role="page" id="americanRcps">').appendTo('body');
+			$('<section data-role="content">').appendTo('#americanRcps');
+				$('<header data-role="header" data-position="fixed"></header>').appendTo('#americanRcps section');
+					$('<h1>American</h1>').appendTo('#americanRcps section header');
+		$('<footer data-role="footer" data-position="fixed"></footer>').appendTo('#americanRcps')
+			$('<nav data-role="navbar">.').appendTo('#americanRcps footer');
+				$('<ul>').appendTo('#americanRcps footer nav');
+					$('<li><a href="#home">Home</a></li>').appendTo('#americanRcps footer nav ul');
+					$('<li><a href="#add">Add</a></li>').appendTo('#americanRcps footer nav ul');
+					$('<li><a href="#view">View</a></li>').appendTo('#americanRcps footer nav ul');
+	}); */
 	/* $('<ul>').appendTo('#home section');
 	// Creates ul for categories in the homepage and appends it to the page
-	$('<li><a href="#american">American</a></li>').appendTo('#home section ul');
-	// Creates the li and anchor tags for the individual categories and appends them to the ul */
+	$('<li><a href="#american">American</a></li>').appendTo('#home section ul'); */
+	// Creates the li and anchor tags for the individual categories and appends them to the ul
 	return false;
 });
 // END Method
