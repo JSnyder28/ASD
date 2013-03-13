cba047f2bc862de0270d5d9f14ff2e70461a4d50
Joshua Snyder <jsnyder28@fullsail.edu>
2013-03-13 15:53:31 -0500

    Created code in the main.js file, and then deleted it.

--- js/main.js
+++ js/main.js
@@ -2,63 +2,28 @@
 // ASD 1303
 // Project Week 1
 
-$(document).ready(function() {
 	
-	$('#home').on('pageinit', function() {
-
-		return false;
-	});
-	// END Method
-
-	/*$('#home').on('pageinit', function() {
-		$('#american').on('click', function() {
-			$('<section data-role="page" id="americanRcps">').appendTo('body');
-				$('<section data-role="content">').appendTo('#americanRcps');
-					$('<header data-role="header" data-position="fixed"></header>').appendTo('#americanRcps section');
-						$('<h1>American</h1>').appendTo('#americanRcps section header');
-			$('<footer data-role="footer" data-position="fixed"></footer>').appendTo('#americanRcps')
-				$('<nav data-role="navbar">.').appendTo('#americanRcps footer');
-					$('<ul>').appendTo('#americanRcps footer nav');
-						$('<li><a href="#home">Home</a></li>').appendTo('#americanRcps footer nav ul');
-						$('<li><a href="#add">Add</a></li>').appendTo('#americanRcps footer nav ul');
-						$('<li><a href="#view">View</a></li>').appendTo('#americanRcps footer nav ul');
-		}); */
-		/* $('<ul>').appendTo('#home section');
-		// Creates ul for categories in the homepage and appends it to the page
-		$('<li><a href="#american">American</a></li>').appendTo('#home section ul'); */
-		// Creates the li and anchor tags for the individual categories and appends them to the ul
-		return false;
-	});
-	// END Method
-
-	$("#newRcpForm").on("pageinit", function() {
-		//When form page loads, validate function is active.
-
-		return false;
-	});
-	// END Method
-
-	$("#addIt").on("click", function() {
-		//Saves current data from input fields of form into storage.
-
-		return false;
-	});
-	// END Method
-
-	$("resetIt").on("click", function() {
-		//Resets all input in form fields.
-
-		return false;
-	});
-	// END Method
-
-
-	$("#resetAll").on("click", function() {
-		//Clears all currently saved data.
-
-		return false;
-	});
-	// END Method
+$('#home').on('pageinit', function() {
 
 });
-// END DOM ready
+// END
+
+$('#add').on('pageinit', function() {
+	// When the add recipe page is ready, run this code.
+
+});
+
+// END
+
+$('#view').on('pageinit', function() {
+	// When the view recipes page is ready, run this code.
+	$('clearAll').on('click', function() {
+		// Clears all saved recipes, and returns to the homepage.
+	});
+
+	$('editRcps').on('click', function() {
+		// Allows individual recipes to be edited and updated.
+	});
+
+});
+// END
