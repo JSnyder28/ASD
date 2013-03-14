d6f3a44bd8d767ca6671cf51c5c63dcf22cea91b
Joshua Snyder <jsnyder28@fullsail.edu>
2013-03-14 18:29:04 -0400

    Can't get the data saved in local storage to display.

--- js/main.js
+++ js/main.js
@@ -9,68 +9,60 @@ $('#home').on('pageinit', function() {
 // END
 
 $('#add').on('pageinit', function() {
-	// When the add recipe page is ready, run this code.
-	$('form').on('submit', function() {
-	// Form submission code.
-		var id = Math.floor(Math.random()*100000001);
-		 	recipe = $(this).serializeArray();
-		localStorage.setItem(id, JSON.stringify(recipe));
-		// Serializes the data from the form input fields, generates a random id, and saves the id and object, to local storage, as json data.
-		console.log(recipe);
-		
-	return false;
-		
-	});
-	
-	//$('<h1></h1>').appendTo('#add header')
-		//.text('Add Recipe!')
-	//;
-	// Creates the h1 tag, appends it to the header, and inserts the text for the heading.
+    // When the add recipe page is ready, run this code.
+    
+    $('form').on('click', function(event) {
+
+        var data     = $(this).serializeArray();
+        var id       = Math.floor(Math.random()*100000001);
+        console.log(data);
+        console.log(id);
+        localStorage.setItem(id, JSON.stringify(data));
+        console.log(data);
+
+        event.preventDefault();    
+    });
 });
 
 // END
 
 $('#view').on('pageinit', function() {
-	$(this).one('click', function() {
-	//$('<div><h2>Test</h2></div>').appendTo('#view section')
-	//	.attr('id', 'item')
-	//	.append('<ul>')
-	//;
-	$('<div>').appendTo('#view section')
-		.attr('id', 'items')
-	;
-	$('<ul>').appendTo('#view section div');
-	$(localStorage).each(function() {
-		$('<li>').appendTo('#view section div ul');
-		var key = localStorage.key($(this));
-			value = localStorage.getItem(key);
-			obj = JSON.parse(value);
-			console.log(obj[0]);
-			$('<ul>').appendTo('#view section div ul li');
-			$('<li>').appendTo('#view section div ul li ul');
-			$('#view section div ul li ul li').append($(obj).map(function() {
-				return $(this).val();
-			}).get().join(", "));
-		//$('<ul>').appendTo('#view section div ul li');
-		//	$(obj).each(function() {
-		//	console.log(obj);
-		//		$('<li>').appendTo('#view section div ul li ul');
+    
+    $('#view').on('click', function() {
 
-		//	});
-	console.log();
-	console.log(key);
-	console.log(value);
-	console.log(obj);
-	});
-	// When the view recipes page is ready, run this code.
-	$('clearAll').on('click', function() {
-		// Clears all saved recipes, and returns to the homepage.
-		localStorage.clear();
-	});
+        $(localStorage).each(function(){
 
-	$('editRcps').on('click', function() {
-		// Allows individual recipes to be edited and updated.
-	});
-});
+            $('<ul>').appendTo('#view > section');
+
+            var key = localStorage.key($(this));           
+            var value = localStorage.getItem(key);           
+            var recipes = jQuery.parseJSON(value);
+           
+            console.log(key);
+            console.log(value);
+            console.log(recipes);
+
+            $(recipes).each(function() {
+
+                $('<li>').append(jQuery.type([]))
+                        .appendTo('#view > section > ul')
+
+                ;
+
+
+            });
+
+        });
+
+    });
+
+
+
+
+
+
+    $('editRcps').on('click', function() {
+        // Allows individual recipes to be edited and updated.
+    });
 });
 // END
