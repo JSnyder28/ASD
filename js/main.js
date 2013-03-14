8f01a908882123462b81c6d877d54f553395f1e5
Joshua Snyder <jsnyder28@fullsail.edu>
2013-03-13 23:52:02 -0500

    Local storage clears. Nearly done with retrieving and displaying data from local storage.

--- js/main.js
+++ js/main.js
@@ -10,7 +10,7 @@ $('#home').on('pageinit', function() {
 
 $('#add').on('pageinit', function() {
 	// When the add recipe page is ready, run this code.
-	$('form').on('submit', function(event) {
+	$('form').on('submit', function() {
 	// Form submission code.
 		var id = Math.floor(Math.random()*100000001);
 		 	recipe = $(this).serializeArray();
@@ -31,16 +31,46 @@ $('#add').on('pageinit', function() {
 // END
 
 $('#view').on('pageinit', function() {
-	$('<ul>').before($('#view input').appendTo($('#view section')));
+	$(this).one('click', function() {
+	//$('<div><h2>Test</h2></div>').appendTo('#view section')
+	//	.attr('id', 'item')
+	//	.append('<ul>')
+	//;
+	$('<div>').appendTo('#view section')
+		.attr('id', 'items')
+	;
+	$('<ul>').appendTo('#view section div');
+	$(localStorage).each(function() {
+		$('<li>').appendTo('#view section div ul');
+		var key = localStorage.key($(this));
+			value = localStorage.getItem(key);
+			obj = JSON.parse(value);
+			console.log(obj[0]);
+			$('<ul>').appendTo('#view section div ul li');
+			$('<li>').appendTo('#view section div ul li ul');
+			$('#view section div ul li ul li').append($(obj).map(function() {
+				return $(this).val();
+			}).get().join(", "));
+		//$('<ul>').appendTo('#view section div ul li');
+		//	$(obj).each(function() {
+		//	console.log(obj);
+		//		$('<li>').appendTo('#view section div ul li ul');
 
+		//	});
+	console.log();
+	console.log(key);
+	console.log(value);
+	console.log(obj);
+	});
 	// When the view recipes page is ready, run this code.
 	$('clearAll').on('click', function() {
 		// Clears all saved recipes, and returns to the homepage.
+		localStorage.clear();
 	});
 
 	$('editRcps').on('click', function() {
 		// Allows individual recipes to be edited and updated.
 	});
-
+});
 });
 // END
