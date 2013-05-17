b79d34b02b27bc7099a827edd654675b69d97c09
Joshua Snyder <jsnyder28@fullsail.edu>
2013-05-16 23:17:25 -0400

    Save and View work correctly. Had delete working, but isn't anymore.

--- js/main.js
+++ js/main.js
@@ -2,10 +2,11 @@
 // ASD 1305
 // Project Week 2
 
+
 $('#home').on('pageinit', function() {
-	$('jsonData').on('click', function() {
-		$.getJSON('xhr/list.js', function(data) {
-            console.log(data.Recipes[1].Category);
+    $('#jsonData').on('click', function() {
+        // When JSON button is clicked
+        $.getJSON('xhr/list.js', function(data) {
             for (var i = 0, len = data.Recipes.length; i < len; i++) {
                 var arr = data.Recipes[i];
                 $(''+'<div>'
@@ -24,334 +25,60 @@ $('#home').on('pageinit', function() {
         });
     });
 
-	$('#xmlData').on('click', function() {
+    $('#xmlData').on('click', function() {
         // When XML button is clicked
-        /* $.ajax({
-            url: "ajax_xml.xml",
-            type: "GET",
-            dataType: "json",
-            success: function(response) {
-                console.log(response);
-            }
-        }); */
-        /* xmlhttp=new XMLHttpRequest();
-        if (window.XMLHttpRequest) {
-            xmlhttp=new XMLHttpRequest();
-        } else {
-            xmlhttp=new ActiveXObject ('Microsoft.XMLHTTP');
-        };
-        xmlhttp.open('GET', 'xhr/list.xml', false);
-        xmlhttp.send();
-        xmlDoc=xmlhttp.responseXML; */
         $('#view > section').load('xhr/list.xml', function(data, status) {
-            console.log(data, status);
         });
     });
-    // END xmlData.on 'click'
 });
+// END #home 'pageinit'
 
-
-
-
-// FIRST edit code
-/* $('#add').on('pageinit', function() {
-	$('#addButton').on('click', function(itemKey) {
-		if(!itemKey) {
-			id = Math.floor(Math.random()*100000001);
-		} else {
-			id = itemKey;
-		}
-		var data = $('form').serializeArray();
-		localStorage.setItem(id, JSON.stringify(data));
-		window.location.reload();
-		console.log(data);
-		console.log(id);
-	});
-});
-
-
-
-$('#view').on('pageinit', function() {
-	$.each(localStorage, function(key, value) {
-		console.log(key);
-		console.log(value);
-		var obj = JSON.parse(value);
-		console.log(value);
-		console.log(value);
-		itemKey = key;
-		console.log(itemKey);
-		var list = $('<ul>Recipe: ' + itemKey + '</ul>')
-						.attr('data-key', itemKey)
-						;
-		$('#displayDiv').append(list);
-		for(var l in obj) {
-			console.log(obj[l]);
-			console.log(obj[l].name);
-			console.log(obj[l].value);
-			var subLi = $('<li>' + obj[l].name + ': ' + obj[l].value + '</li>');
-			$(list).append(subLi);
-		};
-		var editItemLi = $('<li>');
-		$(list).append(editItemLi);
-		var editItemAnchor = $('<a>').attr("href", "#editPage")
-									.html("Edit")
-									;
-		$(editItemLi).append(editItemAnchor);
-
-		var deleteItemLi = $('<li>');
-		$(list).append(deleteItemLi);
-		var deleteItemAnchor = $('<a>').attr("href", "#")
-									   .html("Delete")
-									   ;
-		$(deleteItemLi).append(deleteItemAnchor);
-
-		$(deleteItemAnchor).on('click', function() {
-			localStorage.removeItem(key);
-			window.location.reload();
-		});
-	});
-
-
-	$('#autoFill').on('click', function() {
-		$.each(json, function(value) {
-			var id = Math.floor(Math.random()*100000001);
-			localStorage.setItem(id, JSON.stringify(value));
-			window.location.reload();
-		});
-	});
-
-
-	$('#clearAll').on('click', function() {
-		localStorage.clear();
-		window.location.reload();
-	});
-});
-
-
-
-$('#editPage').on('pageinit', function(itemKey){
-	
-}); */
-
-
-
-
-
-
-
-
-
-// ORIGINAL code
-/* $('#add').on('pageinit', function() {
-    
-        var storeData = function(key) {
-            // Check for already existent key.
-            if (!key) {
-                // Generate a random key number, assigning to id variable.
-                var id = Math.floor(Math.random()*100000001);
-            } else {
-                // Overwrite data being edited.
-                // Store new data with current key.
-                id = key;
-            }
-            var item = $('form').serializeArray();
-            localStorage.setItem(id, JSON.stringify(item));
-        };
-    $('#addButton').on('click', storeData);
-    $('#cancelLink').on('click', function() {
-
+$('#add').on('pageinit', function() {
+    $('form').on('submit', function() {
+        var id = Math.floor(Math.random()*100000001);
+        var data = $(this).serializeArray();
+        localStorage.setItem(id, JSON.stringify(data));
     });
 });
+// END #add 'pageinit'
 
 $('#view').on('pageinit', function() {
-    for(i = 0, j = localStorage.length; i < j; i++) {
-        var makeLi = $('<li>');
-        var makeLinksLi = $('<li>');
-        $('#itemsList').append(makeLi);
-        var key = localStorage.key(i);
-        console.log(key);
-        var value = localStorage.getItem(key);
-            console.log(value);
-        var obj = JSON.parse(value);
-            console.log(obj);
-        var makeList = $('<ul>');
-        $(makeLi).append(makeList);
-        for(var l in obj) {
-            console.log(obj[0].name);
-            var makeSubLi = $('<li>');
-            $(makeList).append(makeSubLi);
-            var text = obj[l].name + ": " + obj[l].value;
-            console.log(text.value);
-            $(makeSubLi).html(text);
-            $(makeList).append(makeLinksLi);
+    $.each(localStorage, function(key, value) {
+        var parArr = JSON.parse(value);
+        $('#displayDiv').append('<ul data-key='+ key +'>Recipe: ' + key + '</ul>');
+        for(var l in parArr) {
+            $('<li>' + parArr[l].name + ": " + parArr[l].value + '</li>'
+             ).appendTo('#displayDiv')
+            ;
         };
-    itemLinks(localStorage.key(i), makeLinksLi);
-    };
+        $('#displayDiv').append('<li>' + '<a href="#add" id="editLink">Edit</a>' + '</li>');
+        
+        $('#displayDiv').append('<li>' + 
+                                    '<a href="#" id="deleteLink">Delete</a>' + 
+                                '</li>'
+                                );
+    });
+
+    $('#deleteLink').on('click', function() {
+        $.each(localStorage, function(key) {
+            console.log(key);
+        });
+        //localStorage.removeItem(key);
+        console.log(key);
+    });
 
     $('#autoFill').on('click', function() {
-            $.each(json, function(key, value) {
-                var id = Math.floor(Math.random(key)*100000001);
-                localStorage.setItem(id, JSON.stringify(value));
-                window.location.reload();
-            });
-            // END .each 'json'
-    });
-    // END #autoFill.on 'click'
-
-    $('#clearAll').on('click', function() {
-        localStorage.clear();
-        window.location.reload();
-    });
-});
-
-var itemLinks = function(key, makeLinksLi) {
-    var editAnchor = $('<a>Edit</a>').attr(
-          'href', '#add',
-          'id', 'editItem',
-          'key', key
-        );
-    $('#editItem').on('click', editItem);
-    console.log(editAnchor);
-    console.log(key);
-    
-    $(makeLinksLi).append(editAnchor)
-                  .append('<br>')
-                  ;
-
-    var deleteAnchor = $('<a>Delete</a>').attr(
-          'href', '#',
-          'id', 'deleteItem',
-          'key', key
-        );
-    $('#deleteItem').on('click', deleteItem);
-    $(makeLinksLi).append(deleteAnchor);
-};
-
-var editItem = function() {
-    $('#addButton').attr('id', 'updateButton');
-    var value = localStorage.getItem(this.key);
-    var item = JSON.parse(value);
-    $('#updateButton').val('Update');
-    $('#updateButton').on('click', storeData(this.key))
-                   .attr('key', this.key)
-                  ;
-};
-
-var deleteItem = function() {
-    var ask = confirm("Are you sure you would like to delete this recipe?");
-    if(ask) {
-        localStorage.removeItem(this.key);
-        window.location.reload();
-    } else {
-        alert("Recipe was not deleted");
-    };
-}; */
-
-
-
-
-
-// JS HERO edited code
-$('#add').on('pageinit', function() {    
-    var storeData = function(key) {
-        // Check for already existent key.
-        if (!key) {
-            // Generate a random key number, assigning to id variable.
+        for (var n in json) {
             var id = Math.floor(Math.random()*100000001);
-        } else {
-            // Overwrite data being edited.
-            // Store new data with current key.
-            id = key;
+            localStorage.setItem(id, JSON.stringify(json[n]));
         };
-        var item = $('form').serializeArray();
-        localStorage.setItem(id, JSON.stringify(item));
-    };
-    $('#addButton').on('click', storeData());
-    $('#cancelLink').on('click', function() {
-
+            window.location.reload();
     });
-});
-
-$('#view').on('pageinit', function(itemLinks) {
-    for(var i = 0, j = localStorage.length; i < j; i++) {
-        var makeLi = $('<li>');
-        var makeLinksLi = $('<li>');
-        $('#itemsList').append(makeLi);
-        var key = localStorage.key(i);
-        console.log(key);
-        var value = localStorage.getItem(key);
-            console.log(value);
-        var obj = JSON.parse(value);
-            console.log(obj);
-        var makeList = $('<ul>');
-        $(makeLi).append(makeList);
-        for(var l in obj) {
-            console.log(obj[0].name);
-            var makeSubLi = $('<li>');
-            $(makeList).append(makeSubLi);
-            var text = obj[l].name + ": " + obj[l].value;
-            console.log(text.value);
-            $(makeSubLi).html(text);
-            $(makeList).append(makeLinksLi);
-        };
-    function itemLinks(localStorage.key(i), makeLinksLi);
-    };
-
-    $('#autoFill').on('click', function() {
-            $.each('json', function(key, value) {
-                var id = Math.floor(Math.random(key)*100000001);
-                localStorage.setItem(id, JSON.stringify(value));
-                window.location.reload();
-            });
-            // END .each 'json'
-    });
-    // END #autoFill.on 'click'
 
     $('#clearAll').on('click', function() {
         localStorage.clear();
         window.location.reload();
     });
 });
+// END #view 'pageinit'
 
-function itemLinks(key, makeLinksLi, editItem, deleteItem) {
-    var editAnchor = $('<a>Edit</a>').attr(
-          'href', '#add',
-          'id', 'editItem',
-          'key', key
-        );
-    $('#editItem').on('click', editItem());
-    console.log(editAnchor);
-    console.log(key);
-    
-    $(makeLinksLi).append(editAnchor)
-                  .append('<br>')
-                  ;
-
-    var deleteAnchor = $('<a>Delete</a>').attr(
-          'href', '#',
-          'id', 'deleteItem',
-          'key', key
-        );
-    $('#deleteItem').on('click', deleteItem());
-    $(makeLinksLi).append(deleteAnchor);
-};
-
-var editItem = function(storeData) {
-    $('#addButton').attr('id', 'updateButton');
-    var value = localStorage.getItem(this.key);
-    var item = JSON.parse(value);
-    $('#updateButton').val('Update');
-    $('#updateButton').on('click', storeData(this.key))
-                   .attr('key', this.key)
-                  ;
-};
-
-var deleteItem = function() {
-    var ask = confirm("Are you sure you would like to delete this recipe?");
-    if(ask) {
-        localStorage.removeItem(this.key);
-        window.location.reload();
-    } else {
-        alert("Recipe was not deleted");
-    };
-};
